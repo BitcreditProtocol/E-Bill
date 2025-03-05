b1bf6cf2037f542e0009bb3e646017475c48a98e
tompro <office@protom.eu>
2025-03-05 16:15:12 +0100

    Retry queue

--- crates/bcr-ebill-transport/src/event/bill_events.rs
+++ crates/bcr-ebill-transport/src/event/bill_events.rs
@@ -1,4 +1,7 @@
-use bcr_ebill_core::notification::ActionType;
+use bcr_ebill_core::{
+    blockchain::bill::{BillBlock, BillBlockchain},
+    notification::ActionType,
+};
 use serde::{Deserialize, Serialize};
 
 /// Can be used for all events that are just signalling an action
@@ -10,4 +13,18 @@ pub struct BillActionEventPayload {
     pub bill_id: String,
     pub action_type: ActionType,
     pub sum: Option<u64>,
+}
+
+/// Used to signal a change in the blockchain of a bill and an optional
+/// action event. Given some bill_id, this can signal an action to be
+/// performed by the receiver and a change in the blockchain. If the
+/// recipient is a new chain participant, the recipient receives the full
+/// chain otherwise just the most recent block.
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct BillChainEventPayload {
+    pub bill_id: String,
+    pub action_type: Option<ActionType>,
+    pub sum: Option<u64>,
+    pub block: Option<BillBlock>,
+    pub chain: Option<BillBlockchain>,
 }
